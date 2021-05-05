834f208495a268ebbd5e2bafee0b57effd4ea9ec
Haseeb-pyy <haa3397@gmail.com>
2021-05-05 20:33:53 +0500

    Updated Javascript
    Added functionality to check email Validation, check length validation and also match password match validation

--- Project1/script.js
+++ Project1/script.js
@@ -4,7 +4,7 @@ const form = document.getElementById('form');
 const userName = document.getElementById('name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const confrm = document.getElementById('conform');
+const password2 = document.getElementById('conform');
 
 // function for show error
 function showError(input , message){
@@ -22,45 +22,77 @@ function showSucess(input){
     const formContol = input.parentElement;
     formContol.className = 'form-control sucess'
 }
+// function to check if required field have data or not
+function checkRequired(inputArray){
+    inputArray.forEach(function(input){
+        if(input.value === '')
+        {
+            showError(input,`${getFieldId(input)} is Required`);
+        }
+        else {
+            showSucess(input);
+        }
+    });
+}
+
+// function to get the id of the input field 
+
+function getFieldId(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// function to show is Email valid
+
+function checkEmail(input){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim()))
+    {
+        showSucess(input);
+    }
+    else {
+        showError(input,`this is not a valid Email`);
+    }
+
+}
+
+// function to match password and password2
+
+function checkPassword(input1,input2)
+{
+    if(input1.value !== input2.value)
+    {
+        showError(input2,`Password is not the same`);
+    }
+    else{
+        showSucess(input);
+    }
+}
 
 
+// function to check character at least 3,10 (in user name) and 6 to 30 at (password)
+function checkLength(input,min,max)
+{
+    if (input.value.length < min)
+    {
+        showError(input,`${getFieldId(input)} is more than ${min}`)
+    }
+    else if (input.value.length > max)
+    {
+        showError(input,`${getFieldId(input)} is less than ${max}`)
+    }
+    else{
+        showSucess(input);
+    }
+}
 
 //--Event Listner
 form.addEventListener('submit',function(e){
     //stop page from reloading
     e.preventDefault();
 
-    if(userName.value === '')
-    {
-        showError(userName,'User Name Required');
-    }
-    else{
-        showSucess(userName);
-    }
-    // for Email
-    if(email.value === '')
-    {
-        showError(email,'Email Name Required');
-    }
-    else{
-        showSucess(email);
-    }
-    //for Password
-    if(password.value === '')
-    {
-        showError(password,'password Name Required');
-    }
-    else{
-        showSucess(password);
-    }
-
-    // for cnfrmation
-    if(confrm.value === '')
-    {
-        showError(confrm,'Conformation Required');
-    }
-    else{
-        showSucess(confrm);
-    }
-
+    checkRequired([userName,email,password,password2]);
+    checkLength(userName,3,10);
+    checkLength(password,6,20);
+    checkEmail(email);
+    checkPassword(password,password2);
 });
