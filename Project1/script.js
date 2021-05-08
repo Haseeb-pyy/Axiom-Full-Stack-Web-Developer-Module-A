fdbfbd86a44b9aaf57ea016b453a26599264d49a
Haseeb-pyy <haa3397@gmail.com>
2021-05-09 02:31:35 +0500

    Add pre loader

--- Project1/script.js
+++ Project1/script.js
@@ -96,3 +96,22 @@ form.addEventListener('submit',function(e){
     checkEmail(email);
     checkPassword(password,password2);
 });
+
+
+let numberCount = 0;
+let counter = setInterval(() => {
+    if(numberCount < 101){
+        const number = document.querySelector('.number');
+        const barline = document.querySelector('.barline');
+        number.textContent = `${numberCount}%`
+        barline.style.width = `${numberCount}%`; 
+        numberCount++;
+    }
+    else{
+        clearInterval(counter);
+        const loading = document.querySelector('.loading');
+        loading.classList.add('bhaag');
+    }
+     //niche jo 40 likha hai uska mtlb 5 seconds hai wahan time barha sakte hein
+}, 40)
+
